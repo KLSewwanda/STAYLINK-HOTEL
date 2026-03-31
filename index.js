a0d51a7554c6a8c221030d7db1248ce90778e8fc
KLSewwanda <sewwandalakshitha28@gmail.com>
2026-04-01 01:24:20 +0530

    configure MongoDB connection, add DNS settings, and enable dotenv for sensitive data

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import dns from "node:dns"
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+dotenv.config()
+
+const mongoDBURI = process.env.MONGO_URI
+
+mongoose.connect(mongoDBURI).then(
+    () => {
+        console.log("Connected mongoDB successfully")
+    }
+)
 
 const app = express()
 app.use(express.json())
