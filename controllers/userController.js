16abfeb6305c91e2accf4489d0ec1386345913b5
KLSewwanda <sewwandalakshitha28@gmail.com>
2026-04-02 19:33:56 +0530

    add bcrypt for password hashing in user creation✏️

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js"
+import bcrypt from "bcrypt"
 
 export async function createUser(req, res) {
     try {
@@ -7,11 +8,13 @@ export async function createUser(req, res) {
         res.status(400).json({ message : "User already exists" })
         return
     }
+    const passwordHash = bcrypt.hashSync(req.body.password, 10)
+
     const newUser = new User({
         email : req.body.email,
         firstName : req.body.firstName,
         lastName : req.body.lastName,
-        password : req.body.password,
+        password : passwordHash,
     })
     await newUser.save()
     res.status(201).json({ message : "User created successfully" })
@@ -21,3 +24,4 @@ export async function createUser(req, res) {
         res.status(500).json({ message : err.message })
     }
 }
+
