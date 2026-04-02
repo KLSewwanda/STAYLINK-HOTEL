bfe950128a6249839bafa6d1061d37391dc0d219
KLSewwanda <sewwandalakshitha28@gmail.com>
2026-04-02 19:51:03 +0530

    add login functionality to user controller and router ✏️

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,34 @@ export async function createUser(req, res) {
     }
 }
 
+export async function loginUser(req, res) {
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        if(email == null || password == null) {
+            res.status(400).json({ message : "Email and password are required" })
+            return
+        }
+        const user = await User.findOne({email : email})
+
+        if(user == null) {
+            res.status(400).json({ message : "User not found" })
+            return
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password)
+        
+        if(isPasswordValid) {
+
+            res.status(200).json({ message : "Login successful" })
+
+        } else {
+
+            res.status(400).json({ message : "Invalid password" })
+            
+        }
+    }catch (err) {
+        res.status(500).json({ message : err.message })
+    }  
+}
